0fb4d580a44732e2a0b280e34ba8358ea2a0096d
JordaoViktor <jordaovpinheiro@gmail.com>
2022-11-29 01:59:36 -0300

    :wrench: svg transformer setup

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -10,6 +10,7 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { useThemeAwareObject } from '@hooks/style/useThemeAwareObject';
 
 import { Button } from '@components/Button';
+import YoutubeLogo from '@assets/svg/YouTube-Logo.svg';
 import { createStyles } from './styles';
 import { RootStackParamListType } from '../../@types/navigation';
 
@@ -27,7 +28,7 @@ export const LoginScreen = () => {
       style={Styles.container}
     >
       <Text style={Styles.mainTitle}>Login</Text>
-
+      <YoutubeLogo width={200} height={200} />
       <View style={Styles.signinWrapper}>
         <Button
           text="Sign in"
